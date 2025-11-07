0ac93a25d24d0f46f1ddc3041901c96faebbf161
r.mkmakukemi <r.mkmakukemi@gmail.com>
2025-11-07 16:46:52 +0000

    Add order item display

--- lib/main.dart
+++ lib/main.dart
@@ -120,3 +120,15 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class OrderItemDisplay extends StatelessWidget {
+  final String itemType;
+  final int quantity;
+
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('This is a placeholder for OrderItemDisplay');
+  }
+}
